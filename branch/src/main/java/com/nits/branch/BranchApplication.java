db18426ae8262a5638dab7a3a7d3aad9d85538c1
govinda <govinda@GOVINDA>
2024-03-05 13:51:03 +0530

    fgf

--- branch/src/main/java/com/nits/branch/BranchApplication.java
+++ branch/src/main/java/com/nits/branch/BranchApplication.java
@@ -10,7 +10,7 @@ public class BranchApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BranchApplication.class, args);
 	}
-	
+
 	@Bean
 	public RestTemplate restTemplate()
 	{
