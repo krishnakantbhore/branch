062a76a13c32c39e82f29422b80a1aa1d43718ef
govinda <govinda@GOVINDA>
2024-03-05 13:46:39 +0530

    init

--- branch/src/main/java/com/nits/branch/BranchApplication.java
+++ branch/src/main/java/com/nits/branch/BranchApplication.java
@@ -1,5 +1,4 @@
 package com.nits.branch;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
